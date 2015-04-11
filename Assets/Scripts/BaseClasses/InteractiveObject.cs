534a66660904143d78e1c8e6e1f3ca22ced24e50
ALeX-L <alxlight@mail.ru>
2015-04-11 17:26:49 +0300

    Start
    Сачальный проект с тестовой сценой и несколькими скриптами

---
+++ Assets/Scripts/BaseClasses/InteractiveObject.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : MonoBehaviour
+{
+
+    public string interactionContainerTag;
+    protected bool used;
+
+    void Awake()
+    {
+        used = false;
+    }
+
+	void OnTriggerEnter2D(Collider2D otherObject) 
+	{
+	    if (!Global.isPaused())
+	    {
+	        if (otherObject.tag == interactionContainerTag)
+	        {
+	            onEnterCollide();
+	        }
+	    }
+	}
+
+	void OnTriggerExit2D(Collider2D otherObject ) 
+	{
+	    if (!Global.isPaused())
+	    {
+	        if (otherObject.tag == interactionContainerTag)
+	        {
+	            onEndCollide();
+	        }
+	    }
+	}
+
+	void  OnTriggerStay2D(Collider2D otherObject)
+	{
+	    if (!Global.isPaused())
+	    {
+	        if (otherObject.name == interactionContainerTag)
+	        {
+	            onCollide();
+	        }
+	    }
+	}
+
+    protected void onEnterCollide()
+    { }
+
+    protected void onCollide()
+    {}
+
+    protected void onUse()
+    {}
+
+    protected void onEndCollide()
+    {}
+
+}
